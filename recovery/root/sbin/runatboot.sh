8d11a515b094929b5fb27412ee859ab6cf11653d
SGCMarkus <markusornik@gmail.com>
2019-07-19 15:01:46 +0200

    judypn: update ramdisk for crypto

---
+++ recovery/root/sbin/runatboot.sh
@@ -0,0 +1,40 @@
+#!/sbin/sh
+
+# The below variables shouldn't need to be changed
+# unless you want to call the script something else
+SCRIPTNAME="VendorInit"
+LOGFILE=/tmp/recovery.log
+
+# Functions for logging to the recovery log
+log_info()
+{
+	echo "I:$SCRIPTNAME:$1" >> "$LOGFILE"
+}
+
+log_error()
+{
+	echo "E:$SCRIPTNAME:$1" >> "$LOGFILE"
+}
+
+# Functions to update props using resetprop
+update_product_device()
+{
+	log_info "Current product: $product"
+	resetprop "ro.build.product" "$1"
+	product=$(getprop ro.build.product)
+	log_info "New product: $product"
+	log_info "Current device: $device"
+	resetprop "ro.product.device" "$1"
+	device=$(getprop ro.product.device)
+	log_info "New device: $device"
+}
+
+update_model()
+{
+	log_info "Current model: $model"
+	resetprop "ro.product.model" "$1"
+	model=$(getprop ro.product.model)
+	log_info "New model: $model"
+}
+
+exit 0
